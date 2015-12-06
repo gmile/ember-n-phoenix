5caf07b8ad331cefce6173564bdb368556833b2c
Eugene Pirogov <iamexile2@gmail.com>
2015-12-06 11:17:05 +0200

    Render actual json

--- server/web/views/city_view.ex
+++ server/web/views/city_view.ex
@@ -2,6 +2,12 @@ defmodule Server.CityView do
   use Server.Web, :view
 
   def render("index.json", %{cities: cities}) do
-    cities
+    %{data: render_many(cities, Server.CityView, "city.json")}
+  end
+
+  def render("city.json", %{city: city}) do
+    %{id: city.id,
+      name: city.name,
+      population: city.population}
   end
 end
